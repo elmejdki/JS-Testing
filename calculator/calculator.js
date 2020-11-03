5cf15f63191a17faa36ae1d52322b4e0014a8543
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-11-03 22:44:19 +0100

    Update Calculator file to export an object

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,22 +1,19 @@
-const add = (...args) => (
-  args.reduce((accumulator, current) => accumulator + current)
-);
+const calculator = {
+  add: (...args) => (
+    args.reduce((accumulator, current) => accumulator + current)
+  ),
 
-const subtract = (...args) => (
-  args.reduce((accumulator, current) => accumulator - current)
-);
+  subtract: (...args) => (
+    args.reduce((accumulator, current) => accumulator - current)
+  ),
 
-const multiply = (...args) => (
-  args.reduce((accumulator, current) => accumulator * current)
-);
+  multiply: (...args) => (
+    args.reduce((accumulator, current) => accumulator * current)
+  ),
 
-const divide = (...args) => (
-  args.reduce((accumulator, current) => accumulator / current)
-);
-
-export {
-  add,
-  subtract,
-  multiply,
-  divide,
+  divide: (...args) => (
+    args.reduce((accumulator, current) => accumulator / current)
+  ),
 };
+
+export default calculator;
--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,39 +1,37 @@
-import {
-  add, subtract, multiply, divide,
-} from './calculator';
+import calculator from './calculator';
 
 describe('Negative test cases', () => {
   test('5 plus 5 is not equal to 3', () => {
-    expect(add(5, 5)).not.toBe(3);
+    expect(calculator.add(5, 5)).not.toBe(3);
   });
 
   test('10 minus 7 is not equal to 6', () => {
-    expect(subtract(10, 7)).not.toBe(6);
+    expect(calculator.subtract(10, 7)).not.toBe(6);
   });
 
   test('10 divide by 5 is not equal to 21', () => {
-    expect(divide(10, 5)).not.toBe(21);
+    expect(calculator.divide(10, 5)).not.toBe(21);
   });
 
   test('5 multiplied with 5 is not equal to 23', () => {
-    expect(multiply(5, 5)).not.toBe(23);
+    expect(calculator.multiply(5, 5)).not.toBe(23);
   });
 });
 
 describe('Positive test cases', () => {
   test('5 plus 5 is equal to 10', () => {
-    expect(add(5, 5)).toBe(10);
+    expect(calculator.add(5, 5)).toBe(10);
   });
 
   test('10 minus 7 is equal to 3', () => {
-    expect(subtract(10, 7)).toBe(3);
+    expect(calculator.subtract(10, 7)).toBe(3);
   });
 
   test('10 divide by 5 is equal to 2', () => {
-    expect(divide(10, 5)).toBe(2);
+    expect(calculator.divide(10, 5)).toBe(2);
   });
 
   test('5 multiplied with 5 is equal to 25', () => {
-    expect(multiply(5, 5)).toBe(25);
+    expect(calculator.multiply(5, 5)).toBe(25);
   });
 });
